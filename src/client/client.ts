7e601225d6f57deabe6904523b57dc2fb1cb290f
tadams95 <tyrelleadams28@gmail.com>
2023-02-03 17:00:23 -0800

    First Commit, Sun Model loaded

---
+++ src/client/client.ts
@@ -0,0 +1,89 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import Stats from "three/examples/jsm/libs/stats.module";
+
+const scene = new THREE.Scene();
+// scene.add(new THREE.AxesHelper(5))
+
+// const light = new THREE.SpotLight()
+// light.position.set(5, 5, 5)
+// scene.add(light)
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 2;
+
+const renderer = new THREE.WebGLRenderer();
+renderer.physicallyCorrectLights = true;
+renderer.shadowMap.enabled = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+const loader = new GLTFLoader();
+loader.load(
+  "models/THE3SUN.GLTF",
+  function (gltf) {
+    gltf.scene.traverse(function (child) {
+      if ((child as THREE.Mesh).isMesh) {
+        const m = child as THREE.Mesh;
+        m.receiveShadow = true;
+        m.castShadow = true;
+      }
+      if ((child as THREE.Light).isLight) {
+        const l = child as THREE.Light;
+        l.castShadow = true;
+        l.shadow.bias = -0.003;
+        l.shadow.mapSize.width = 2048;
+        l.shadow.mapSize.height = 2048;
+      }
+    });
+    scene.add(gltf.scene);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  (error) => {
+    console.log(error);
+  }
+);
+
+window.addEventListener("resize", onWindowResize, false);
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
+}
+
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  const time = -performance.now() * 0.00003;
+
+  camera.position.x = 1.5 * Math.cos(time);
+  camera.position.z = 1.5 * Math.sin(time);
+  
+  camera.lookAt(scene.position);
+
+  render();
+  controls.update();
+  stats.update();
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+animate();
